4ef9b32806b2235fb7143bbe9b03a488416d7970
Mutsuhiro <21per.nm@gmail.com>
2019-08-09 02:33:07 +0900

    add declaration header file

---
+++ sort.h
@@ -0,0 +1,13 @@
+/* sort algorithms with int array */
+
+/** bubble sort **/
+void bubble_sort(int *A, int size);
+
+/* insertion sort */
+void insertion_sort(int *A, int size);
+
+/* selection sort */
+void selection_sort(int *A, int size);
+
+/* quick sort*/
+void quick_sort(int *A, int size);
