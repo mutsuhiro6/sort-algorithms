945ca3744cdd38ad4df0a0d353c15fce33091dfd
Mutsuhiro <21per.nm@gmail.com>
2019-08-09 17:26:58 +0900

    remove main function

--- bubble-sort.c
+++ bubble-sort.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "util.h"
+#include "sort.h"
 
 void bubble_sort(int *A, int size)
 {
@@ -15,19 +16,4 @@ void bubble_sort(int *A, int size)
             }
         }
     }
-}
-
-int main()
-{
-    int A[5] = {9, 7, 5, 3, 2};
-    int size = sizeof(A) / sizeof(0);
-    int i;
-    printf("before: ");
-    print_array(A, size);
-
-    bubble_sort(A, size);
-
-    printf("after: ");
-    print_array(A, size);
-    return 0;
 }
