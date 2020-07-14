e34711228e50839daad7bf66b259a2a363e4d7aa
Mutsuhiro Iwamoto <iwamoto@miubiq.cs.titech.ac.jp>
2020-07-15 05:17:07 +0900

    fix test procedure and add tests

--- test_sort.c
+++ test_sort.c
@@ -1,25 +1,83 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 #include "sort.h"
 #include "util.h"
 
-int test_bubble_sort()
-{
-    printf("\nbubble sort\n");
-    int A[5] = {9, 7, 5, 3, 2};
-    int size = sizeof(A) / sizeof(0);
-    int i;
-    printf("before: ");
-    print_array(A, size);
+int ANSWER[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int INPUT[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int size = sizeof(INPUT) / sizeof(int);
 
-    bubble_sort(A, size);
+int test_merge_sort()
+{
+    shuffle_array(INPUT, size);
+
+    printf("\nMERGE SORT\n");
+    printf("before:");
+    print_array(INPUT, size);
+
+    merge_sort(INPUT, size);
 
     printf("after: ");
-    print_array(A, size);
-    return 0;
+    print_array(INPUT, size);
+    return memcmp(INPUT, ANSWER, sizeof(INPUT));
+}
+
+int test_insertion_sort()
+{
+    shuffle_array(INPUT, size);
+
+    printf("\nINSERTION SORT\n");
+    printf("before:");
+    print_array(INPUT, size);
+
+    insertion_sort(INPUT, size);
+
+    printf("after: ");
+    print_array(INPUT, size);
+    return memcmp(INPUT, ANSWER, sizeof(INPUT));
+}
+
+int test_selection_sort()
+{
+    shuffle_array(INPUT, size);
+
+    printf("\nSELECTION SORT\n");
+    printf("before:");
+    print_array(INPUT, size);
+
+    selection_sort(INPUT, size);
+
+    printf("after: ");
+    print_array(INPUT, size);
+    return memcmp(INPUT, ANSWER, sizeof(INPUT));
+}
+
+int test_bubble_sort()
+{
+    shuffle_array(INPUT, size);
+
+    printf("\nBUBBLE SORT\n");
+    printf("before: ");
+    print_array(INPUT, size);
+
+    bubble_sort(INPUT, size);
+
+    printf("after: ");
+    print_array(INPUT, size);
+    return memcmp(INPUT, ANSWER, sizeof(INPUT));
 }
 
 int main(void)
 {
-    test_bubble_sort();
+    srand((unsigned int)time(NULL));
+    if (test_bubble_sort() != 0)
+        printf("ERROR in Bubble sort.\n");
+    if (test_selection_sort() != 0)
+        printf("ERROR in Selection sort.\n");
+    if (test_insertion_sort() != 0)
+        printf("ERROR in Insertion sort.\n");
+    if (test_merge_sort() != 0)
+        printf("ERROR in Merge sort.\n");
     return 0;
 }
